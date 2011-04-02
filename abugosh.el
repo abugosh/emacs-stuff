ac3bf5276b5bc1d0bfaf37aee0d717238b310cb7
Alex Bugosh <abugosh@gmail.com>
2011-04-02 11:32:33 -0400

    grrr

--- abugosh.el
+++ abugosh.el
@@ -58,14 +58,6 @@
 (load (concat dotfiles-dir "vendor/ess/lisp/ess-site.el"))
 (require 'ess-site)
 
-;; Full screen toggle
-(defun toggle-fullscreen ()
-  (interactive)
-  (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
-                                           nil
-                                         'fullboth)))
-(global-set-key (kbd "M-n") 'toggle-fullscreen)
-
 (defalias 'qrr 'query-replace-regexp)
 
 ;;
@@ -140,51 +132,18 @@
 
 (global-set-key (kbd "RET") 'newline-and-indent)
 
+;; Fullscreen keybinding
+(global-set-key (kbd "M-RET") 'ns-toggle-fullscreen)
+
+;; Change the command key into meta since this version of emacs wants
+;; it to be alt :(
+(setq ns-command-modifier 'meta)
+
 ;; Other
 
 (prefer-coding-system 'utf-8)
 
 (server-start)
 
-;;; Another Theme
-(defun color-theme-blue ()
-  "Color theme by hosiawak, created 2007-03-15."
-  (interactive)
-  (color-theme-install
-   '(color-theme-blue
-     ((background-color . "#162433")
-      (background-mode . dark)
-      (border-color . "black")
-      (cursor-color . "yellow")
-      (foreground-color . "#C7D4E2")
-      (mouse-color . "sienna1"))
-     (default ((t (:background "black" :foreground "white"))))
-     (blue ((t (:foreground "blue"))))
-     (bold ((t (:bold t))))
-     (bold-italic ((t (:bold t))))
-     (border-glyph ((t (nil))))
-     (buffers-tab ((t (:background "black" :foreground "white"))))
-     (font-lock-builtin-face ((t (:foreground "white"))))
-     (font-lock-comment-face ((t (:foreground "#428BDD"))))
-     (font-lock-constant-face ((t (:foreground "#00CC00"))))
-     (font-lock-doc-string-face ((t (:foreground "DarkOrange"))))
-     (font-lock-function-name-face ((t (:foreground "white"))))
-     (font-lock-keyword-face ((t (:foreground "#F9BB00"))))
-     (font-lock-preprocessor-face ((t (:foreground "Aquamarine"))))
-     (font-lock-reference-face ((t (:foreground "SlateBlue"))))
-     (font-lock-string-face ((t (:foreground "#00CC00"))))
-     (font-lock-type-face ((t (:foreground "white"))))
-     (font-lock-variable-name-face ((t (:foreground "white"))))
-     (font-lock-warning-face ((t (:bold t :foreground "Pink"))))
-     (gui-element ((t (:background "#D4D0C8" :foreground "black"))))
-     (highlight ((t (:background "darkolivegreen"))))
-     (highline-face ((t (:background "SeaGreen"))))
-     (italic ((t (nil))))
-     (left-margin ((t (nil))))
-     (text-cursor ((t (:background "yellow" :foreground "black"))))
-     (toolbar ((t (nil))))
-     (underline ((nil (:underline nil))))
-     (zmacs-region ((t (:background "snow" :foreground "ble")))))))
-;;; Theme end
-
+;; Zenburn rocks!
 (color-theme-zenburn)
