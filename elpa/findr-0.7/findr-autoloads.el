d6ec9c03a6c021930c310367270a15d084cf0fc3
Alex Bugosh <abugosh@gmail.com>
2011-09-22 21:00:36 -0400

    Now with the elpa stuff

---
+++ elpa/findr-0.7/findr-autoloads.el
@@ -0,0 +1,16 @@
+;;; findr-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (19546 2101
+;;;;;;  253640))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; findr-autoloads.el ends here
